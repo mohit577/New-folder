51e645258b62ea6a5f3c93a9701f907809cfba33
mohit <mk7738771@gmail.com>
2022-05-08 14:44:12 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,167 @@
+ <?php
+//echo 'My';
+//echo 'Name';
+//echo 'is';
+//echo 'Alok';
+
+
+// $name = array(
+//     'Alok',
+//     'Amit',
+//     'Anuj',
+//     'Harsh',
+//     'Mohit',
+//     'Ashu',
+//     'Someone'
+// );
+
+//var_dump($name);
+//print_r($name);
+//echo $name[0];
+
+// $mix = array(
+//     'name' => 'Alok',
+//     'age' => '23',
+//     'gender' => 'male'
+// );
+
+//var_dump($mix);
+
+//print_r($mix);
+
+//echo $mix['name'];
+
+//Datatypes in php.
+
+// $string = 'jslfjdf0938rh9fef8j';
+// $integer = 23483947832;
+// $float = 23.3434;
+// $boolean = true;
+// $array = array();
+// $null = null;
+
+// #concatination..
+
+// $first_name = 'Monit';
+// $last_name = 'Singh';
+
+//echo $first_name.' '.$last_name;
+
+#this is comment also.
+
+//PrintThisVar('this is going to be printed', 'hello world');
+
+/**
+ * This function is being used to print the passed variable;
+ *
+ * @param $data - This is the value which will be printed.
+ * @param $null - This will be a null value.
+ * @return void
+ */
+// function PrintThisVar($data, $null) {
+//     echo $data;
+//     echo $null;
+// }
+
+
+#loops
+
+#4 types of loop in php
+#1 = While
+#2 = For
+#3 = Foreach loop
+#4 Do while loop.
+
+
+// $status = false;
+
+// while ($status !== true) {
+//     echo 'after this value will become true';
+//     echo '<br>';
+//     $status = true;
+// }
+
+// $count = 10;
+
+// for ($num = 0; $num <= $count; $num++) {
+//     echo $num.'<br>';
+// }
+// $coun = 1;
+// do {
+//     echo 'this is happening'.'<br>';
+//     $coun++;
+// } while ($coun <= 5);
+
+
+// foreach ($name as $key=>$person) {
+//     echo 'The '.$key.' of this person is '.$person.'<br>';
+// }
+
+// $arrayAsso = array(
+//     'users' => array(
+//         'Alok',
+//         'Anuj'
+//     ),
+//     'gender' => array(
+//         'male',
+//         'female'
+//     ),
+//     'address' => array(
+//         'house' => array(
+//             '223',
+//             '342'
+//         ),
+//         'street' => array(
+//             '7',
+//             'shardanand colony',
+//         )
+//     )
+// );
+
+// echo $arrayAsso['address']['street'][1];
+// $data = json_encode($arrayAsso);
+
+// echo '<br>';
+// echo '<br>';
+// echo '<br>';
+// echo '<br>';
+// echo '<br>';
+// echo $data; -->
+
+
+
+
+// $int =100;
+// for($j=1;$j<=100;$j++)
+// echo '<br>'.$j;
+
+
+// $i =0;
+// while ($i <= 100) {
+//     echo $i.'<br>';
+//     $i++;
+// }
+// $i =0;
+// do 
+//    {
+//      echo $i.'<br>'; 
+//      $i++;
+//    } while ($i<= 100);
+$details = array(
+
+    'name' =>'Mohit kumar singh',
+    'age' => '21',
+    'study' => 'BCA',
+    'father' => 'shayamkant singh',
+    
+  
+);
+var_dump($details);
+echo '<br>';
+
+
+foreach ($details as $key => $member)
+ {
+  echo $key.'='.$member,'<br>' ;    
+}
+
